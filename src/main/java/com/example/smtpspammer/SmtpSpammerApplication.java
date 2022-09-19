0ffd356588f6f7d2bb629beec840d0269a06820b
Thanh TranV <thanh.tranv@sai-digital.com>
2022-09-19 15:15:54 +0700

    Initial commit

---
+++ src/main/java/com/example/smtpspammer/SmtpSpammerApplication.java
@@ -0,0 +1,13 @@
+package com.example.smtpspammer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmtpSpammerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SmtpSpammerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/smtpspammer/SmtpSpammerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.smtpspammer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmtpSpammerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
